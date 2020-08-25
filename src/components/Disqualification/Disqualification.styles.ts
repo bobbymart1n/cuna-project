8623919f4eccbf9f446b4a2d5d0c625d32471e2a
Bobby Martin <bobby@mixmax.com>
2020-08-25 16:07:30 -0700

    fix: add style to disqualification component

--- src/components/Disqualification/Disqualification.styles.ts
+++ src/components/Disqualification/Disqualification.styles.ts
@@ -1,1 +1,9 @@
 import styled from 'styled-components';
+
+const StyledDisqualification = styled.div`
+  max-width: 474px;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+export { StyledDisqualification }
--- src/components/Disqualification/Disqualification.tsx
+++ src/components/Disqualification/Disqualification.tsx
@@ -1,9 +1,12 @@
 import React, { FC } from 'react';
 
+import { StyledDisqualification } from './Disqualification.styles';
+
 const Disqualification: FC = () => (
-  <div>
-    Disqualified
-  </div>
+  <StyledDisqualification>
+    <h1>I'm sorry, you don't qualify at this time.</h1>
+    <p>Please contact customer server at <a href='tel:555-867-5309'>555-867-5309</a></p>
+  </StyledDisqualification>
 );
 
 export default Disqualification;
