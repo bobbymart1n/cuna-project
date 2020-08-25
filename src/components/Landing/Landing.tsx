837d4a6d716f68b234fffa193f5eb9e03c51b674
Bobby Martin <bobby@mixmax.com>
2020-08-25 12:58:48 -0700

    feat: add landing component files

---
+++ src/components/Landing/Landing.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+import { StyledLanding } from './Landing.styles';
+
+const Landing: FC = () => (
+  <StyledLanding>
+    Landing
+  </StyledLanding>
+);
+
+export default Landing;
