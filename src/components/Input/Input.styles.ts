0ea8decabf97696bfef21ef748d054ccaf060c41
Bobby Martin <bobby@mixmax.com>
2020-08-25 15:46:46 -0700

    improvement: add input styles

--- src/components/Input/Input.styles.ts
+++ src/components/Input/Input.styles.ts
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
 
-const StyledInput = styled.input``;
+const StyledInput = styled.input`
+  width: 100%;
+  height: 60px;
+  padding: 0;
+  margin-top: 20px;
+  border-radius: 4px;
+  border: 2px solid #000000;
+  font-size: 18px;
+  padding-left: 16px;
+
+  &:first-child {
+    margin-top: 0;
+  }
+`;
 
 export { StyledInput };
