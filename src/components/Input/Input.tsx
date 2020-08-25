9e67d537f6fcbce1cabe27d546e2357668153639
Bobby Martin <bobby@mixmax.com>
2020-08-25 15:14:34 -0700

    fix: add type to input and remove onchange

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,8 +2,14 @@ import React, { FC } from 'react';
 
 import { StyledInput } from './Input.styles';
 
-const Input: FC = () => (
-  <StyledInput type='text' />
+type Props = {
+  placeholder?: string;
+  name: string;
+  type: string;
+}
+
+const Input: FC<Props> = ({ placeholder, name, type }) => (
+  <StyledInput type={type} placeholder={placeholder} name={name} required />
 );
 
-export default Input
+export default Input;
