e1db0a8590eeec9e85edc73ff6276e6b52a90bc8
Bobby Martin <bobby@mixmax.com>
2020-08-25 15:46:35 -0700

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Landing from './components/Landing/Landing';
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Landing />
+        </Route>
+        <Route path='/disqualified'>
+          {/* Disqualified component here */}
+        </Route>
+        <Route path='/new-account'>
+          {/* New account component here */}
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
