792659ffb4d07727b8728394ee09e2b455c14937
Bobby Martin <bobby@mixmax.com>
2020-08-25 22:29:03 -0700

    chore: change button to use new component

--- src/components/Landing/Landing.styles.ts
+++ src/components/Landing/Landing.styles.ts
@@ -15,16 +15,4 @@ const StyledLandingMarketingText = styled.p`
   margin: 20px 0;
 `;
 
-const StyledLandingButton = styled.button`
-  background-color: #396AC9;
-  width: 100%;
-  height: 60px;
-  margin-top: 20px;
-  border: none;
-  border-radius: 4px;
-  font-size: 18px;
-  font-weight: 600;
-  color: #FFFFFF;
-`;
-
-export { StyledLanding, StyledLandingButton, StyledLandingHeading, StyledLandingMarketingText }
+export { StyledLanding, StyledLandingHeading, StyledLandingMarketingText }
--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,9 +1,10 @@
 import React, { FC, SyntheticEvent, useReducer } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import Button from '../Button/Button';
 import Input from '../Input/Input';
 
-import { StyledLanding, StyledLandingButton, StyledLandingHeading, StyledLandingMarketingText } from './Landing.styles';
+import { StyledLanding, StyledLandingHeading, StyledLandingMarketingText } from './Landing.styles';
 
 const Landing: FC = () => {
   const history = useHistory();
@@ -44,12 +45,12 @@ const Landing: FC = () => {
   const onSubmit = async (event: SyntheticEvent) => {
     event.preventDefault();
 
-    // I would use fetch here with a POST method, but to keep things brief, I've left this as a resolved promise.
     let data;
-
+    
     if (parseInt(state.price, 10) > 1000000) {
       data = await Promise.reject(console.error('Bad Request'))
     } else {
+      // I would use fetch here with a POST method, but to keep things brief, I've left this as a resolved promise.
       data = await Promise.resolve(state);
     }
     
@@ -70,7 +71,7 @@ const Landing: FC = () => {
         <Input type='text' placeholder='Auto Model' name='model' />
         <Input type='number' placeholder='Estimated Yearly Income' name='income' />
         <Input type='number' placeholder='Estimated Credit Score' name='creditScore' />
-        <StyledLandingButton type='submit'>Calculate</StyledLandingButton>
+        <Button type='submit' text='Calculate' />
       </form>
     </StyledLanding>
   );
