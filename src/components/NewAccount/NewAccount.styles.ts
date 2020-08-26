c216ed30557d9369c91d408d77749c1eaf0e2a88
Bobby Martin <bobby@mixmax.com>
2020-08-25 22:29:29 -0700

    feat: add new account component with logic

---
+++ src/components/NewAccount/NewAccount.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledNewAccount = styled.form`
+  max-width: 474px;
+  margin: 0 auto;
+`;
+
+export { StyledNewAccount }
---
+++ src/components/NewAccount/NewAccount.tsx
@@ -0,0 +1,69 @@
+import React, { FC, SyntheticEvent, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import Button from '../Button/Button';
+import Input from '../Input/Input';
+
+import { StyledNewAccount } from './NewAccount.styles';
+
+const NewAccount: FC = () => {
+  const [ state, setState ] = useState({});
+  const [ validEmail, setValidEmail ] = useState(true);
+  const [ validPassword, setValidPassword ] = useState(true);
+  const history = useHistory();
+  
+  const usernameValidation = (username: string) => {
+    // Basic regex that looks for typical email address format, this definitely isn't the best regex for checking this
+    const usernameRegex = new RegExp('^([A-Za-z0-9_\\-\\.])+\\@([A-Za-z0-9_\\-\\.])+\\.([A-Za-z]{2,4})');
+
+    const usernameVal = usernameRegex.test(username);
+    
+    return setValidEmail(usernameVal)
+  }
+
+  const passwordValidation = (password: string) => {
+    // Create a new regular expression that looks for length, character, and symbols without those used by regex
+    const passwordRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\\$%\\^&\\*])(?=.{8,})');
+    
+    const passwordVal = passwordRegex.test(password)
+
+    if (passwordVal && state.confirmPassword === password) {
+      return setValidPassword(passwordVal)
+    }
+  }
+
+  const onChange = (event: SyntheticEvent) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    if (name === 'password') {
+      passwordValidation(value)
+    } else {
+      usernameValidation(value)
+    }
+
+    setState({
+      ...state,
+      [name]: value
+    })
+  }
+
+  const onSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    return history.push('/success');
+  }
+
+  return (
+    <StyledNewAccount onChange={onChange} onSubmit={onSubmit}>
+      <h1>Create a new account:</h1>
+      <Input type='text' placeholder='Username' name='username' hasError={state.username && state.username.length > 0 && !validEmail} />
+      <Input type='password' placeholder='Password' name='password' hasError={state.password && state.password.length > 0 && !validPassword} />
+      <Input type='password' placeholder='Confirm Password' name='confirmPassword' hasError={state.confirmPassword && state.confirmPassword.length > 0 && state.password !== state.confirmPassword} />
+      <Button type='submit' text='Submit' />
+    </StyledNewAccount>
+  )
+}
+
+export default NewAccount;
